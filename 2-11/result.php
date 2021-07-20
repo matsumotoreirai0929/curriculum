5eb09caf09e56e27db54c0d9acd861524511c68e
matsumotoreirai0929 <87117357+matsumotoreirai0929@users.noreply.github.com>
2021-07-20 22:20:50 +0900

    2-11
    2-11

---
+++ 2-11/result.php
@@ -0,0 +1,8 @@
+<?php
+$my_name = $_POST['my_name'];
+$mail = $_POST['my_mail'];
+$password = $_POST['password'];
+?>
+<p>私の名前は、<?php echo $my_name; ?></p>
+<p>メールアドレスは、<?php echo $mail; ?></p>
+<p>私のパスワードは、<?php echo $password; ?></p>
